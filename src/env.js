36c1f0d24c67610bdaeed9e9df9ae57b8323dcba
Vijayan Raghavan <vijayan@Priyeshs-Mac.local>
2023-11-06 18:54:05 +0530

    base

--- src/env.js
+++ src/env.js
@@ -1,8 +1,8 @@
 export const VAR = {
   //============== BASE URL =============================
-  // BASE_URL: "https://isdb.sharepoint.com/sites/ssc",
+  BASE_URL: "https://isdb.sharepoint.com/sites/ssc",
   // BASE_URL: "http://localhost:3000",
-  BASE_URL: "http://localhost:3456",
+  // BASE_URL: "http://localhost:3456",
   REACT_APP_BUILD_URL: "/quality/IDBSocialClub",
   //====+==================================================
 
